9d63be291a471887034bb17fc9973ccd2f62cf68
seuh-h <stu4704@gmail.com>
2026-05-13 15:07:23 +0900

    auto save: 2026-05-13 15:07:23.84

--- push.bat
+++ push.bat
@@ -2,7 +2,11 @@
 cd /d "%~dp0"
 git add .
 git commit -m "auto save: %date% %time%"
-git push
+git push origin feature/alba
+git checkout main
+git merge feature/alba --no-edit
+git push origin main
+git checkout feature/alba
 echo.
-echo Done! GitHub updated.
+echo Done! GitHub updated (feature/alba + main).
 pause
